c31e1e9b90798b122d762353bbed5c37370ef361
Karan Joshi <karanjoshi31@live.com>
2018-03-05 19:45:44 -0600

    restarting project from scratch

--- TwitchIntegration.py
+++
@@ -1,9 +0,0 @@
-#Twitch client ID information
-import requests
-
-#Client ID for this application
-headers = {'Client-ID' :'5hi6d1chqez0e845sfgqrec0ls2e0n'}
-url = 'https://api.twitch.tv/helix/users?login=ronmoz'
-r = requests.get(url, headers=headers)
-
-print(r.text)
